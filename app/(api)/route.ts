3a7f2357abdfa092c70bdee36145001867700420
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-18 21:37:23 +0530

    removed other methods form the / route

--- app/(api)/route.ts
+++ app/(api)/route.ts
@@ -1,22 +1,9 @@
 import { responseHandler } from "@/utils";
-import { NextRequest, NextResponse } from "next/server";
 
-export const GET = (_req: NextRequest, _res: NextResponse) => {
+export const GET = () => {
   try {
     return responseHandler.successHandler();
   } catch (error) {
     return responseHandler.serverErrorHandler(error);
   }
-};
-
-export const POST = (_req: NextRequest, _res: NextResponse) => {
-  return responseHandler.notFoundHandler();
-};
-
-export const PUT = (_req: NextRequest, _res: NextResponse) => {
-  return responseHandler.notFoundHandler();
-};
-
-export const DELETE = (_req: NextRequest, _res: NextResponse) => {
-  return responseHandler.notFoundHandler();
 };
