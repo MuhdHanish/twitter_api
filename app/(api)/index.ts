abe3c0b23fc7c4cd9b5d7ba6272a0b4773a6418a
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-18 21:28:13 +0530

    removed instace file

--- app/(api)/index.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponseHandler } from "@/lib/providers/responseHandler/implementations/NextResponseHandler";
-
-const responseHandler = new NextResponseHandler();
-
-export { responseHandler };
