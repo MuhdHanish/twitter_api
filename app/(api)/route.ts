b0a73ec08654933cf21387fd055141e8aea6613a
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-19 08:06:24 +0530

    removed not found handler function form /route.ts

--- app/(api)/route.ts
+++ app/(api)/route.ts
@@ -1,12 +1,5 @@
 import { responseHandler } from "@/utils";
 
-export const handleNotFound = () => {
-  try {
-    return responseHandler.notFoundHandler();
-  } catch (error) {
-    return responseHandler.serverErrorHandler(error);
-  }
-}
 export const GET = () => {
   try {
     return responseHandler.successHandler();
