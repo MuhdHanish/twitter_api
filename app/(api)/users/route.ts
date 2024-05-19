952f10caef8b15cceccc8b1f3386d364c640ee6d
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-19 07:17:24 +0530

    added /user get handler and getting the users from the db

--- app/(api)/users/route.ts
+++ app/(api)/users/route.ts
@@ -1,10 +1,15 @@
+import prisma from "@/prisma";
 import { responseHandler } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = (_req: NextRequest, _res: NextResponse) => {
+export const GET = async(_req: NextRequest, _res: NextResponse) => {
   try {
-    return responseHandler.successHandler();
+    await prisma.$connect();
+    const users = await prisma.user.findMany();
+    return responseHandler.successHandler(users);
   } catch (error) {
     return responseHandler.serverErrorHandler(error);
+  } finally {
+    await prisma.$disconnect();
   }
 };
