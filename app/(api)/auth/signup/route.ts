9ad3f0c3f9d27ae5e72a6ae8851828641efd9f57
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-19 08:04:18 +0530

    added the email unique error handling

--- app/(api)/auth/signup/route.ts
+++ app/(api)/auth/signup/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/prisma";
 import * as bcrypt from "bcryptjs";
-import { responseHandler } from "@/utils";
+import { Prisma } from "@prisma/client";
+import { CustomError, responseHandler } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest, _res: NextResponse) => {
@@ -9,7 +10,6 @@ export const POST = async (req: NextRequest, _res: NextResponse) => {
     if (!name || !email || !password) {
       return responseHandler.customHandler(
         ["name is required", "email is required", "password is required"],
-        null,
         400
       );
     }
@@ -18,8 +18,11 @@ export const POST = async (req: NextRequest, _res: NextResponse) => {
     const { password: storedPassword, ...user } = await prisma.user.create({
       data: { name, email, password: hashedPassword }
     });
-    return responseHandler.createHandler(user);
+    return responseHandler.customHandler("User Registered Successfully", 201, user);
   } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+      return responseHandler.serverErrorHandler(new CustomError("Email already exists", 409));
+    }
     return responseHandler.serverErrorHandler(error);
   } finally {
     await prisma.$disconnect();
